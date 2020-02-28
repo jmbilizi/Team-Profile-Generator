1e99b855c35edb6b2ce0884f120a67be2dc49365
Admin <admin@Admins-MacBook-Pro.local>
2020-02-28 12:55:22 -0700

    created files and added Employee class and extended it to Manager, Engineer and Intern class

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,32 @@
+// TODO: Write code to define and export the Employee class
+
+class Employee {
+  constructor(name, id, email) {
+    if (!name) {
+      throw new Error("You are missing the name.");
+    }
+    if (!id) {
+      throw new Error("You are missing the name.");
+    }
+    if (!email) {
+      throw new Error("You are missing the name.");
+    }
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  getName() {
+    return this.name;
+  }
+  getId() {
+    return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return Employee;
+  }
+}
+
+module.exports = Employee;
